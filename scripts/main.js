9a521667510c96c074075bdd557e3388ff9f42a1
Your Name <jessicamarybarnes@gmail.com>
2019-02-17 18:32:26 +0000

    function for valid move works

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const player2 = document.querySelector('.player2')
 
 // .........................CREATE THE GRID ............................
+// .........................ADD EVENT LISTENER ............................
 
 //created a grid array
 
@@ -25,14 +26,13 @@ window.addEventListener('DOMContentLoaded', () => {
       box.className = 'white box'
       //adding an event listener to each box that changes the class to red
       box.addEventListener('click', (e) => {
-        e.target.classList.add('red')
+        validMove(e)
         console.log('row', e.target.getAttribute('data-row'), 'column', e.target.getAttribute('data-column'))
       })
       //adding an attribute of data-row to the box with a value of i
       box.setAttribute('data-row', i)
       //adding an attribute of data-column to the box with a value of x
       box.setAttribute('data-column', x)
-
       // adding the box to the html one by one - makes it display on the page
       gridElement.append(box)
       //pushing the box element to the the row array
@@ -41,26 +41,41 @@ window.addEventListener('DOMContentLoaded', () => {
     //pushing the row to the grid array
     grid.push(row)
   }
-console.log(grid)
-// .........................TOKEN DROP CHECK............................
 
 
-  //a function that checks that the class of box is white and whether the div below is red or black
 
-  const isWhite  = function(e) {
-    if (e.classList.contains('white')) {
-      //to include... if the div below contains red or black {
-      e.classList.remove('white')
-      e.classList.add('red')
+// .........................CHECK FOR VALID MOVE................................
+
+
+  //a function that checks that the class of box is white, then checks if the white
+  //space is on the last column, else checks if there are divs with the class of red and yellow below it.
+  //if not, state that this is not a valid move.
+
+  //call this function when the box is clicked.
+
+  const validMove = function(e) {
+    if (e.target.classList.contains('white')) {
+      if (e.target.getAttribute('data-row') === '5') {
+        e.target.classList.add('red')
+      } else {
+        //check if there are divs below with the class of red or yellow
+        const dataRowString = e.target.getAttribute('data-row')
+        const dataRow = parseInt(dataRowString)
+        const dataColumnString = e.target.getAttribute('data-column')
+        const dataColumn = parseInt(dataColumnString)
+        const boxBelow = grid[dataRow + 1][dataColumn]
+        if (boxBelow.classList.contains('red' || 'yellow')) {
+          e.target.classList.add('red')
+        } else {
+          alert('This is not a valid move. Please click an alternative white space.')
+        }
+      }
     } else {
-      console.log('Not an available move')
+      alert('This is not a valid move. Please click an alternative white space.')
     }
   }
 
 
-  // add attributes to the grid creation where it returns the x, i values of the boxes
-
-  // in a seperate function when a box is clicked, find the x, i values of that and then use that to find the one below.
 
 
 
